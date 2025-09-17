cba3d6f2c73f08c49b7ba777aff494a6684a2473
Ryan Loh <ryanloh28@yahoo.com.sg>
2025-09-17 09:19:45 +0800

    Added sample json string for sse message

--- lib/hachiware/sse/connection.ex
+++ lib/hachiware/sse/connection.ex
@@ -9,7 +9,7 @@ defmodule Hachiware.Sse.Connection do
         Phoenix.PubSub.broadcast(
           Hachiware.PubSub,
           "Darling:HoldMyHand",
-          {Hachiware.PubSub, "Nothing beats a Jet2 holiday"}
+          {Hachiware.PubSub, "{\"parser\": \"hcl2\", \"content\": \"provider \\\"aws\\\" {\\n  region = \\\"ap-southeast-1\\\"\\n}\\nresource \\\"aws_ecr_repository\\\" \\\"scrooge_ecr\\\" {\\n  name                = \\\"scrooge-ecr\\\"\\n\\n  image_scanning_configuration {\\n    scan_on_push = true\\n  }\\n}\\n\"}"}
         )
       end
     end
