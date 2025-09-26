63f6964459effefbc74d67aedf2ac5e1d5c8018b
Zachareee <zacharytanhx@yahoo.com>
2025-09-26 23:47:27 +0800

    Achieved near instant change detection

--- lib/hachiware/poller/runner.ex
+++ lib/hachiware/poller/runner.ex
@@ -1,44 +1,32 @@
 defmodule Hachiware.Poller.Runner do
   use Task
 
-  @spec run(atom()) :: nil
+  @spec run(atom()) :: []
   def run(watched_resource) do
-    current_map =
-      watched_resource
-      |> apply(:retrieve_records, [])
-      |> Stream.map(
-        &{
-          apply(watched_resource, :entry_id, [&1]),
-          {apply(watched_resource, :diff_attribute, [&1]), &1}
-        }
-      )
-      |> Map.new()
+    watched_resource
+    |> apply(:retrieve_records, [])
+    |> Stream.map(
+      &{
+        apply(watched_resource, :entry_id, [&1]),
+        apply(watched_resource, :diff_attribute, [&1]),
+        &1
+      }
+    )
+    |> Stream.each(fn {id, diff, original} ->
+      Hachiware.Poller.Storage.set_stored(fn stored_map ->
+        entries = Map.get(stored_map, watched_resource, %{})
 
-    stored_map = Hachiware.Poller.Storage.get_stored(watched_resource)
+        if Map.get(entries, id) !== diff do
+          Hachiware.Sse.ConnectionImplementation.send(%Hachiware.Sse.ConnectionImplementation{
+            type: apply(watched_resource, :module_name, []),
+            data: Map.drop(original, [:__struct__, :__meta__])
+          })
+        end
 
-    diff =
-      Map.reject(current_map, fn {k, {v, _}} ->
-        Map.has_key?(stored_map, k) && Map.get(stored_map, k) == v
+        Map.put(entries, id, diff)
+        |> then(&Map.put(stored_map, watched_resource, &1))
       end)
-
-    if diff !== %{} do
-      merged =
-        Map.merge(
-          stored_map,
-          diff
-          |> Stream.map(fn {k, {v, _}} -> {k, v} end)
-          |> Map.new()
-        )
-
-      Hachiware.Sse.ConnectionImplementation.send(%Hachiware.Sse.ConnectionImplementation{
-        type: apply(watched_resource, :module_name, []),
-        data:
-          Enum.map(diff, fn {_, {_, m}} ->
-            Map.drop(m, [:__struct__, :__meta__])
-          end)
-      })
-
-      Hachiware.Poller.Storage.set_stored(watched_resource, merged)
-    end
+    end)
+    |> Enum.to_list()
   end
 end
