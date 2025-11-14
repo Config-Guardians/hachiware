d5a8245bf0625ff955134d67cb0c4518e432a3a3
Zachareee <zacharytanhx@yahoo.com>
2025-11-14 17:48:05 +0800

    Update schema

--- gen_migrations.bat
+++ gen_migrations.bat
@@ -1,2 +1,4 @@
 set SIDECAR_HOST=lmao
+del /s /q priv\repo\migrations
+del /s /q priv\resource_snapshots\repo\public.reports
 mix ash_postgres.generate_migrations updated_schema --domains Hachiware.Reports
